27b888cd28867b3fe0b583ab2748fa65a71abbfc
rutujagharge <ghargerl2016@gmail.com>
2025-04-25 11:31:02 +0530

    Episode 2 - Laying foundation - react element and react components and composite component

--- App.js
+++ App.js
@@ -6,9 +6,8 @@
 // // have to put h1 into DOM on to our browser
 // // let create the DOM element to put heading inside DIV tag
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-// // whatever will do we will render inside header using render 
+// // whatever will do we will render inside header using render
 // root.render(heading);
-
 
 // craeting nested elements
 
@@ -19,39 +18,85 @@
  *      </div>
  * </div>
  * ReactElement(Object)  = HTML(Browser understands)
- * 
+ *
  */
 //---------------------------------------------- Eposide 2----------------------------------------
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement(
-        "div",
-        { id: "child1" },
-        // Array Of Children
-        [React.createElement("h1", {}, "I'm an h1 tag of child 1"),
-        React.createElement("h2", {}, "I'm an h2 tag of child 1")]    // we can add more than 1 child in an Array if have to add 2 tags as h1 and h2
-    ),
-    React.createElement(
-        "div",
-        { id: "child2" },
-        // Array Of Children
-        [React.createElement("h1", {}, "I'm an h1 tag of child 2"),
-        React.createElement("h2", {}, "I'm an h2 tag of child 2")]    // we can add more than 1 child in an Array if have to add 2 tags as h1 and h2
-    )]
-);
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     [React.createElement(
+//         "div",
+//         { id: "child1" },
+//         // Array Of Children
+//         [React.createElement("h1", {}, "I'm an h1 tag of child 1"),
+//         React.createElement("h2", {}, "I'm an h2 tag of child 1")]    // we can add more than 1 child in an Array if have to add 2 tags as h1 and h2
+//     ),
+//     React.createElement(
+//         "div",
+//         { id: "child2" },
+//         // Array Of Children
+//         [React.createElement("h1", {}, "I'm an h1 tag of child 2"),
+//         React.createElement("h2", {}, "I'm an h2 tag of child 2")]    // we can add more than 1 child in an Array if have to add 2 tags as h1 and h2
+//     )]
+// );
 
+// console.log(parent)
 
-console.log(parent)
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// whatever will do we will render inside header using render 
-//  It is rendering on to the DOM while rendering it convert itself into that html and puts it up into the DOM
-root.render(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // whatever will do we will render inside header using render
+// //  It is rendering on to the DOM while rendering it convert itself into that html and puts it up into the DOM
+// root.render(parent);
 
 //---------------------------------------------- Eposide 2----------------------------------------
 
 //---------------------------------------------- Eposide 3----------------------------------------
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//jsx element
+const jsxElement = (
+  <div>
+    {" "}
+    {console.log("Hi rutuja from JSX")}{" "}
+    {/** We can print console logs also inside react element / react component HTML code */}
+    <h1> Hello React from JSX 🚀</h1>
+  </div>
+);
+
+// React component
+/**
+ * Class component
+ * Functional component
+ */
+
+// Composite Composition
+const Title = () => <h1>Hello React from Title Component 🚀</h1>;
+
+// JS Variable
+const number = 1000;
+// Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    {number}
+    {/* Inside react component HTML code we can write the Javascript code like this */}
+    <Title />
+    {/** we can write React componenet like below also*/}
+    {Title()}
+    {/** we can write React element inside react component */}
+    {jsxElement}
+    <h1>Hello React from JSX Heading Component 🚀</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxElement);
+console.log("First root - " + root);
+
+// In this case only last render will get print
+root.render(<HeadingComponent />);
+console.log("Second root - " + root);
